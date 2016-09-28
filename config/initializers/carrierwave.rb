6e454a38e0cd2a3a5c1dc79559d144944a8ed8b8
Mezbah Alam <mezbahalam26@gmail.com>
2016-09-28 16:51:32 +0000

    imae

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.fog_credentials = {
       provider:              'AWS', # required
-      aws_access_key_id:     ENV['S3_ACCESS_KEY'], #Rails.application.secrets.aws_access_key_id, # required
-      aws_secret_access_key:  ENV['S3_SECRET_KEY'],#Rails.application.secrets.aws_secret_access_key, # required
+      aws_access_key_id:     Rails.application.secrets.aws_access_key_id, # required
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
       region:                'us-east-1', # optional, defaults to 'us-east-1'
     }
     config.fog_directory  = 'socify' # required
